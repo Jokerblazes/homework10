340cacb662c3d72d896b88ae543e4bf570f51667
Jokerblazes <zcd123@qq.com>
2018-04-22 21:43:09 +0800

    完成GameControllerTest的play测试。测试输入数据格式有误、猜测错误、猜测正确

--- src/test/java/tw/controllers/GameControllerTest.java
+++ src/test/java/tw/controllers/GameControllerTest.java
@@ -2,12 +2,19 @@ package tw.controllers;
 
 import org.junit.Before;
 import org.junit.Test;
+import tw.commands.GuessInputCommand;
+import tw.commands.InputCommand;
+import tw.core.Answer;
 import tw.core.Game;
+import tw.core.GameStatus;
+import tw.core.model.GuessResult;
 import tw.views.GameView;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Arrays;
 
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
@@ -37,6 +44,122 @@ public class GameControllerTest {
             assertTrue(false);
         }
 
+    }
+
+    //测试Play并且猜对
+    @Test
+    public void testPlayAndGuessRight() {
+        String result = "------Please input your answer as x x x x , x <10 ------\n" +
+                "Guess Result: 4A0B\n" +
+                "Guess History:\n" +
+                "[Guess Numbers: 1 2 3 4, Guess Result: 4A0B]\n" +
+                "Game Status: success\n";
+        String in = "1 2 3 4";
+        System.setIn(new ByteArrayInputStream(in.getBytes()));
+        Answer answer = mock(Answer.class);
+        when(answer.toString()).thenReturn(in);
+        when(game.checkCoutinue()).thenReturn(true).thenReturn(false);
+        GuessResult guessResult = new GuessResult("4A0B", answer);
+        when(game.guess(any())).thenReturn(guessResult);
+        when(game.guessHistory()).thenReturn(Arrays.asList(guessResult));
+        when(game.checkStatus()).thenReturn(GameStatus.SUCCESS);
+
+        try {
+            gameController.play(new GuessInputCommand());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertEquals(systemOut(),result);
+    }
+
+
+    //测试Play并且猜错
+    @Test
+    public void testPlayAndGuessFail() {
+        String result = "------Please input your answer as x x x x , x <10 ------\n" +
+                "Guess Result: 3A1B\n" +
+                "Guess History:\n" +
+                "[Guess Numbers: 1 2 3 5, Guess Result: 3A1B]\n" +
+                "------Please input your answer as x x x x , x <10 ------\n" +
+                "Guess Result: 3A1B\n" +
+                "Guess History:\n" +
+                "[Guess Numbers: 1 2 3 5, Guess Result: 3A1B]\n" +
+                "[Guess Numbers: 1 2 3 5, Guess Result: 3A1B]\n" +
+                "------Please input your answer as x x x x , x <10 ------\n" +
+                "Guess Result: 3A1B\n" +
+                "Guess History:\n" +
+                "[Guess Numbers: 1 2 3 5, Guess Result: 3A1B]\n" +
+                "[Guess Numbers: 1 2 3 5, Guess Result: 3A1B]\n" +
+                "[Guess Numbers: 1 2 3 5, Guess Result: 3A1B]\n" +
+                "------Please input your answer as x x x x , x <10 ------\n" +
+                "Guess Result: 3A1B\n" +
+                "Guess History:\n" +
+                "[Guess Numbers: 1 2 3 5, Guess Result: 3A1B]\n" +
+                "[Guess Numbers: 1 2 3 5, Guess Result: 3A1B]\n" +
+                "[Guess Numbers: 1 2 3 5, Guess Result: 3A1B]\n" +
+                "[Guess Numbers: 1 2 3 5, Guess Result: 3A1B]\n" +
+                "------Please input your answer as x x x x , x <10 ------\n" +
+                "Guess Result: 3A1B\n" +
+                "Guess History:\n" +
+                "[Guess Numbers: 1 2 3 5, Guess Result: 3A1B]\n" +
+                "[Guess Numbers: 1 2 3 5, Guess Result: 3A1B]\n" +
+                "[Guess Numbers: 1 2 3 5, Guess Result: 3A1B]\n" +
+                "[Guess Numbers: 1 2 3 5, Guess Result: 3A1B]\n" +
+                "[Guess Numbers: 1 2 3 5, Guess Result: 3A1B]\n" +
+                "Game Status: fail\n";
+        String input = "1 2 3 5\n" +
+                "1 2 3 5\n" +
+                "1 2 3 5\n" +
+                "1 2 3 5\n" +
+                "1 2 3 5\n" +
+                "1 2 3 5";
+        when(game.checkCoutinue()).thenReturn(true).
+                thenReturn(true).thenReturn(true).thenReturn(true).thenReturn(true).thenReturn(false);
+        Answer answer = mock(Answer.class);
+        when(answer.toString()).thenReturn("1 2 3 5");
+        GuessResult guessResult = new GuessResult("3A1B", answer);
+        when(game.guess(any())).thenReturn(guessResult);
+        when(game.checkStatus()).thenReturn(GameStatus.FAIL);
+        when(game.guessHistory()).thenReturn(Arrays.asList(guessResult))
+        .thenReturn(Arrays.asList(guessResult,guessResult))
+        .thenReturn(Arrays.asList(guessResult,guessResult,guessResult))
+        .thenReturn(Arrays.asList(guessResult,guessResult,guessResult,guessResult))
+        .thenReturn(Arrays.asList(guessResult,guessResult,guessResult,guessResult,guessResult))
+        .thenReturn(Arrays.asList(guessResult,guessResult,guessResult,guessResult,guessResult,guessResult));
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        try {
+            gameController.play(new GuessInputCommand());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertEquals(systemOut(),result);
+    }
+
+    //测试输入数据格式错误
+    @Test
+    public void testPlayAndInputError() {
+        String result = "------Please input your answer as x x x x , x <10 ------\n" +
+                "------Please input your answer as x x x x , x <10 ------\n" +
+                "Guess Result: 4A0B\n" +
+                "Guess History:\n" +
+                "[Guess Numbers: 1 2 3 4, Guess Result: 4A0B]\n" +
+                "Game Status: success\n";
+        String input = "1 2\n" +
+                "1 2 3 4";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        when(game.checkCoutinue()).thenReturn(true).thenReturn(false);
+        Answer answer = mock(Answer.class);
+        when(answer.toString()).thenReturn("1 2 3 4");
+        GuessResult guessResult = new GuessResult("4A0B", answer);
+        when(game.guess(any())).thenReturn(guessResult);
+        when(game.guessHistory()).thenReturn(Arrays.asList(guessResult));
+        when(game.checkStatus()).thenReturn(GameStatus.SUCCESS);
+        try {
+            gameController.play(new GuessInputCommand());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assertEquals(systemOut(),result);
     }
 
     private String systemOut() {
