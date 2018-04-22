fd57874af3357c78681a43059300241a084a5cc2
Jokerblazes <zcd123@qq.com>
2018-04-22 15:34:12 +0800

    完成AnswerTest的createAnswer（生成Answer）测试

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -1,8 +1,27 @@
 package tw.core;
 
+import org.junit.Test;
+import tw.core.exception.OutOfRangeAnswerException;
+import tw.core.model.Record;
+
+import java.util.Arrays;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
 /**
  * 在AnswerTest文件中完成Answer中对应的单元测试
  */
 public class AnswerTest {
+    @Test
+    public void testCreateAnswer() {
+        Answer answer = Answer.createAnswer("3 4 5 6");
+        assertEquals(answer.getIndexOfNum("3"),0);
+        assertEquals(answer.getIndexOfNum("4"),1);
+        assertEquals(answer.getIndexOfNum("5"),2);
+        assertEquals(answer.getIndexOfNum("1"),-1);
+    }
 
+   
 }
