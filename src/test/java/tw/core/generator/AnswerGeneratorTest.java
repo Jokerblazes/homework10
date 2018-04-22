a8456b893f7c5085c55b2a0c66fefd09ce68451e
Jokerblazes <zcd123@qq.com>
2018-04-22 15:27:52 +0800

    完成AnswerTest的createAnswer（生成Answer）测试

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,25 @@
 package tw.core.generator;
 
+import org.junit.Test;
+import tw.core.exception.OutOfRangeAnswerException;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
-
+    @Test
+    public void testGenerateSuccess() {
+        RandomIntGenerator randomIntGenerator = mock(RandomIntGenerator.class);
+        when(randomIntGenerator.generateNums(9, 4)).thenReturn("1 2 3 4");
+        AnswerGenerator answerGenerator = new AnswerGenerator(randomIntGenerator);
+        try {
+            answerGenerator.generate();
+            assertTrue(true);
+        } catch (OutOfRangeAnswerException e) {
+            assertTrue(false);
+        }
+    }
 }
 
