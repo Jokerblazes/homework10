2852354cc941a6ad2f708b2daf74cfd730f96b94
Jokerblazes <zcd123@qq.com>
2018-04-22 15:20:36 +0800

    完成RandomIntGeneratorTest测试工作

--- src/test/java/tw/core/RandomIntGeneratorTest.java
+++ src/test/java/tw/core/RandomIntGeneratorTest.java
@@ -1,9 +1,27 @@
 package tw.core;
 
 
+import org.junit.Test;
+import tw.core.generator.RandomIntGenerator;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * 在RandomIntGeneratorTest文件中完成RandomIntGenerator中对应的单元测试
  */
 public class RandomIntGeneratorTest {
-
+    @Test
+    public void testGenerateNums() {
+        RandomIntGenerator randomIntGenerator = new RandomIntGenerator();
+        String result = randomIntGenerator.generateNums(9, 4);
+        Set<Integer> set = new HashSet<>();
+        for (String s : result.split(" ")) {
+            set.add(Integer.parseInt(s));
+        }
+        assertEquals(set.size(),4);
+        set.forEach(integer -> assertEquals(integer >= 0 && integer <= 9,true));
+    }
 }
